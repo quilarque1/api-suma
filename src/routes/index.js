a4a3e7700fed8d54f532a88cf84fe08dde68fedb
Francisco Quilarque <ext_fgquilarque@falabella.cl>
2020-11-26 11:34:03 -0400

    agregando suma service

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const SumaService = require('../services/suma');
 
 router.get('/health', (req, res) => {
+	const sumaService = new SumaService();
+	console.log(sumaService.getSuma('jh'))
 	res.json({ status: "api-suma is Up!!" });
 });
 
---
+++ src/services/suma.js
@@ -0,0 +1,14 @@
+
+class SumaService {
+
+	getSuma(datos) {
+		const suma = sumaDto;
+		return suma || [];
+	}
+}
+const sumaDto = {
+	operacion: '1+2+3+4',
+	resultado: '10'
+};
+
+module.exports = SumaService;
